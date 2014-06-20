dcdd510858ba2f620c2d58da2e1dfb0a6ab9e1bf
Alex Barauskas <a.barauskas@y-node.com>
2014-06-20 17:36:29 +0300

    .

---
+++ public/javascripts/app_module.js
@@ -0,0 +1,28 @@
+CloudFlare.define("invitebox",
+    [       "invitebox/config", "cloudflare/jquery1.7"],
+    function(config,                $)
+    {
+        function addBadge(count){
+            var cdnPath = "//ajax.cloudflare.com/cdn-cgi/nexp/apps/invitebox/";
+            var div = $("<div>");
+            $("<img>",{
+                src: cdnPath + "images/logo-200.png",
+                "class": "countersback"
+            }).appendTo(div);
+            $("<span>", {"style": "position:relative; top:-20px; left:-50px;")
+                .text(count).appendTo(div);
+            div.appendTo("body");
+        }
+
+  /*      $.getJSON(
+            "http://countersback.herokuapp.com/hit?callback=?",
+            {domain_id: config && config.domain_id},
+            function(data){
+                if (this.data.count){
+                    addBadge(this.data.count);
+                }
+            }
+        );
+   */
+    }
+);
